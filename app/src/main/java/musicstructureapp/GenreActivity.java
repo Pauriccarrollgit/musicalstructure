4b9b3d5ebb60f2497a5ffa7e6be58894f3adfe81
Pauric Carroll <pauriccarroll@google.com>
2017-06-27 22:37:24 +1000

    Initial commit

---
+++ app/src/main/java/musicstructureapp/GenreActivity.java
@@ -0,0 +1,31 @@
+package musicstructureapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class GenreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_genre);
+
+        //Get calling intent and set title
+        Intent intent = getIntent();
+        String name = intent.getStringExtra("name");
+        this.setTitle(name);
+
+        //Set "Play Song" button to show detailed view
+        Button play_song_button = (Button) findViewById(R.id.play_song_button);
+        play_song_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(GenreActivity.this, SongDetailActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
